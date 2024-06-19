dcf4eaf91176af34d064e73f38d93c79d0e621c0
Moustafa Hassanin <moustafa.mhassanin@gmail.com>
2024-06-19 14:39:32 +0300

    update changes

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -3,3 +3,10 @@ import concat from 'gulp-concat';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 const sass = gulpSass(dartSass);
+
+//Task
+export const css = () => gulp
+.src('src/font-awesome-pro.scss')
+.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+.pipe(concat('fontAwesomePro.min.css'))
+.pipe(gulp.dest('dist/css'))
